79ea0b92674cd5a2b861a26a24df82e7a6190009
aniket3101 <aniket1999phull@gmail.com>
2023-07-16 21:22:08 -0400

    automation assignment

---
+++ modules/database-N01596244/variables.tf
@@ -0,0 +1,19 @@
+variable "rgroup-name" {
+
+}
+
+variable "rgroup-location" {
+
+}
+
+variable "db_username" {
+  default = "N01596456"
+}
+
+variable "db_pass" {
+  default = "Admin@123!"
+}
+
+variable "db_version" {
+  default = "9.5"
+}
